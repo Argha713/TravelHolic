5af42cf74cbbda3dac35c2d18ca8c7d7fba36a2d
Argha713 <sarkar.argha2@gmail.com>
2023-05-15 11:45:42 +0530

    add navc toggle for mobile view

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,52 @@ const preloader = document.querySelector("[data-preloader]");
 window.addEventListener("load", ()=>{
     preloader.classList.add("remove");
 })
+
+
+/**
+ * Add Event on multiple Element 
+ */
+
+const addEventOnElements = function(elements, eventType, callBack){
+    console.log("addEventOnElements 2");
+    console.log(elements.length?.toString());
+    for(i=0, len=elements.length; i<len; i++){
+        console.log(elements[i].className);
+        elements[i].addEventListener(eventType, callBack);
+    }
+}
+
+
+
+/**
+ * Navbar Toggler For Mobile
+ */
+
+const navBar = document.querySelector("[data-navbar]");
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]");
+
+const toggleNav = function(){
+    navBar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNav);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
